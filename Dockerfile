c368e05a217bacd5e97cbc2984c9d1a9222a92a8
Ed Jeon <muikor@gmail.com>
2019-03-20 18:51:43 +0900

    temp

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,38 @@
-FROM continuumio/miniconda3
+FROM ubuntu:16.04
 
+RUN apt-get update && apt-get install -y curl
+RUN curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+RUN apt-get install -y bzip2
+RUN bash Miniconda3-latest-Linux-x86_64.sh -b && \
+      rm Miniconda3-latest-Linux-x86_64.sh
+
+#RUN conda install python=3.7
 RUN conda install faiss-cpu==1.5.0 -c pytorch
 
 RUN cd && mkdir .pip && echo "[global]\nindex-url=http://ftp.daumkakao.com/pypi/simple\ntrusted-host=ftp.daumkakao.com" > ./.pip/pip.conf
 #RUN sed -i 's/archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 
-ENV GRPC_PYTHON_VERSION 1.19.0
-RUN python -m pip install --upgrade pip
-RUN pip install grpcio==${GRPC_PYTHON_VERSION} grpcio-tools==${GRPC_PYTHON_VERSION}
-
 RUN mkdir -p /app
 WORKDIR /app
 
+RUN python -m pip install --upgrade pip
 RUN pip install pandas
 RUN pip install gevent==1.4.0
 RUN pip install click
 RUN pip install boto3
 RUN pip install --upgrade setuptools 2>/dev/null ; pip install google-cloud-storage
+
+ENV GRPC_PYTHON_VERSION 1.19.0
+RUN apt-get update && apt-get install -y build-essential
+RUN pip install grpcio==${GRPC_PYTHON_VERSION} --no-binary grpcio
+RUN pip install grpcio-tools==${GRPC_PYTHON_VERSION}
+
+# 아래 블럭 정리 필요
+#RUN pip uninstall -y grpcio
+#RUN apt-get update && apt-get install -y python-dev
+#RUN apt-get install -y python3-dev
+#RUN apt-get install -y build-essential
+#RUN pip install grpcio --no-binary grpcio
 
 # for click library
 ENV LC_ALL C.UTF-8
---
+++ server2.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from concurrent import futures
+import contextlib
+import datetime
+import logging
+import math
+import multiprocessing
+import time
+import socket
+import sys
+
+import grpc
+import click
+
+import faissindex_pb2_grpc as pb2_grpc
+from faiss_server import FaissServer
+
+_LOGGER = logging.getLogger(__name__)
+
+_ONE_DAY = datetime.timedelta(days=1)
+_PROCESS_COUNT = multiprocessing.cpu_count()
+_THREAD_CONCURRENCY = _PROCESS_COUNT
+
+def is_prime(n):
+    for i in range(2, int(math.ceil(math.sqrt(n)))):
+        if n % i == 0:
+            return False
+    else:
+        return True
+
+
+class PrimeChecker(FaissServer):
+
+    def check(self, request, context):
+        _LOGGER.info('Determining primality of %s', request.candidate)
+        return prime_pb2.Primality(isPrime=is_prime(request.candidate))
+
+
+def _wait_forever(server):
+    try:
+        while True:
+            time.sleep(_ONE_DAY.total_seconds())
+    except KeyboardInterrupt:
+        server.stop(None)
+
+def _run_server(bind_address):
+    """Start a server in a subprocess."""
+    _LOGGER.info('Starting new server.')
+    options = (('grpc.so_reuseport', 1),)
+
+    # WARNING: This example takes advantage of SO_REUSEPORT. Due to the
+    # limitations of manylinux1, none of our precompiled Linux wheels currently
+    # support this option. (https://github.com/grpc/grpc/issues/18210). To take
+    # advantage of this feature, install from source with
+    # `pip install grpcio --no-binary grpcio`.
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=_THREAD_CONCURRENCY,),
+        options=options
+        )
+    dim = 0
+    save_path = '20190320_060037.index'
+    keys_path = None
+    nprobe = 100
+    servicer = PrimeChecker(dim, save_path, keys_path, nprobe)
+    pb2_grpc.add_ServerServicer_to_server(servicer, server)
+    server.add_insecure_port(bind_address)
+    server.start()
+    _wait_forever(server)
+
+@contextlib.contextmanager
+def _reserve_port():
+    """Find and reserve a port for all subprocesses to use."""
+    import socket
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    if sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT) != 1:
+        raise RuntimeError("Failed to set SO_REUSEPORT.")
+    sock.bind(('', 0))
+    try:
+        yield sock.getsockname()[1]
+    finally:
+        sock.close()
+
+
+def main():
+    with _reserve_port() as port:
+        bind_address = 'localhost:{}'.format(port)
+        _LOGGER.info("Binding to '%s'", bind_address)
+        sys.stdout.flush()
+        workers = []
+        for _ in range(_PROCESS_COUNT):
+            # NOTE: It is imperative that the worker subprocesses be forked before
+            # any gRPC servers start up. See
+            # https://github.com/grpc/grpc/issues/16001 for more details.
+            worker = multiprocessing.Process(
+                target=_run_server, args=(bind_address,))
+            worker.start()
+            workers.append(worker)
+        for worker in workers:
+            worker.join()
+
+
+if __name__ == '__main__':
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('[PID %(process)d] %(message)s')
+    handler.setFormatter(formatter)
+    _LOGGER.addHandler(handler)
+    _LOGGER.setLevel(logging.INFO)
+    main()
